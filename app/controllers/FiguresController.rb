3f32223d9325145516f5c1e77d5f1c25365b8c72
tkoar <terrancekoar@gmail.com>
2017-05-18 18:04:52 -0400

    done.

--- app/controllers/FiguresController.rb
+++ app/controllers/FiguresController.rb
@@ -8,24 +8,52 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
+    @figures = Figure.all
     erb :'/figures/new'
   end
 
   post '/figures' do
     @figure = Figure.create(params["figure"])
-    # binding.pry
-    if params["title"]["name"]
-      @title = title.create(params["title"])
-      @figure.titles << @title
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
-    # @figure.landmarks << @landmark
-    # redirect "/figures/#{@figure.id}"
-    erb :'/figures/show'
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
+    @titles = @figure.titles
+    @landmarks = @figure.landmarks
     erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 
 
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,30 @@
 class LandmarksController < ApplicationController
-  # 
-  # get '/landmarks' do
-  #   @landmarks = Landmark.all
-  #   erb :'/landmarks/index'
-  # end
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
 
+  get '/landmarks/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'/landmarks/new'
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+    erb :'/landmarks/show'
+  end
 end
